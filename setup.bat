70b39b60b430d5843c90148045d49aee01c50c13
kashewnuts <bjzli.m08vo9kqs@gmail.com>
2013-08-25 18:38:29 +0900

    add setting for windows

---
+++ setup.bat
@@ -0,0 +1,7 @@
+mklink %USERPROFILE%\.vimrc %USERPROFILE%\dotfiles\.vimrc
+mklink %USERPROFILE%\.gvimrc %USERPROFILE%\dotfiles\.gvimrc
+mklink %USERPROFILE%\.gitignore %USERPROFILE%\dotfiles\.gitignore
+mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\.gitconfig
+mklink /d %USERPROFILE%\.vim %USERPROFILE%\dotfiles\.vim
+
+pause
