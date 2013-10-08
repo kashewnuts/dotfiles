452e294f7e5ca051a699e561985a138e70773121
kashewnuts <bjzli.m08vo9kqs@gmail.com>
2013-10-09 01:05:20 +0900

    mv bash files

--- setup.bat
+++ setup.bat
@@ -1,12 +1,11 @@
 mklink %USERPROFILE%\.vimrc %USERPROFILE%\dotfiles\vim\.vimrc
 mklink %USERPROFILE%\.gvimrc %USERPROFILE%\dotfiles\vim\.gvimrc
-mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\.gitconfig_win
+mklink /d %USERPROFILE%\.vimfiles %USERPROFILE%\dotfiles\vim\.vim
+mklink /d %USERPROFILE%\.vim %USERPROFILE%\dotfiles\vim\.vim
+mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\.gitconfig
 mklink %USERPROFILE%\.gitignore %USERPROFILE%\dotfiles\.gitignore
 mklink %USERPROFILE%\.hgrc %USERPROFILE%\dotfiles\hg\.hgrc_win
 mklink %USERPROFILE%\.hgignore %USERPROFILE%\dotfiles\hg\.hgignore
-mklink %USERPROFILE%\.hgignore_global %USERPROFILE%\dotfiles\hg\.hgignore_global
 mklink %USERPROFILE%\.pythonstartup %USERPROFILE%\dotfiles\.pythonstartup
-mklink /d %USERPROFILE%\.vimfiles %USERPROFILE%\dotfiles\vim\.vim
-mklink /d %USERPROFILE%\.vim %USERPROFILE%\dotfiles\vim\.vim
 
 pause
--- setup_min.bat
+++
@@ -1,6 +0,0 @@
-mklink %USERPROFILE%\.vimrc %USERPROFILE%\dotfiles\vim\.vimrc
-mklink %USERPROFILE%\.gvimrc %USERPROFILE%\dotfiles\vim\.gvimrc
-mklink /d %USERPROFILE%\vimfiles %USERPROFILE%\dotfiles\vim\.vim
-mklink /d %USERPROFILE%\.vim %USERPROFILE%\dotfiles\vim\.vim
-
-pause
