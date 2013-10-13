68b69f4c59de6c88262085062657cdcfd30da266
kashewnuts <bjzli.m08vo9kqs@gmail.com>
2013-10-14 06:45:28 +0900

    change win

--- setup.bat
+++ setup.bat
@@ -2,8 +2,8 @@ mklink %USERPROFILE%\.vimrc %USERPROFILE%\dotfiles\files\.vimrc
 mklink %USERPROFILE%\.gvimrc %USERPROFILE%\dotfiles\files\.gvimrc
 mklink /d %USERPROFILE%\.vimfiles %USERPROFILE%\dotfiles\files\.vim
 mklink /d %USERPROFILE%\.vim %USERPROFILE%\dotfiles\files\.vim
-mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\.gitconfig
-mklink %USERPROFILE%\.gitignore %USERPROFILE%\dotfiles\.gitignore
+mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\files\.gitconfig_win
+mklink %USERPROFILE%\.gitignore %USERPROFILE%\dotfiles\files\.gitignore
 mklink %USERPROFILE%\.hgrc %USERPROFILE%\dotfiles\files\.hgrc_win
 mklink %USERPROFILE%\.hgignore %USERPROFILE%\dotfiles\files\.hgignore
 mklink %USERPROFILE%\.pythonstartup %USERPROFILE%\dotfiles\files\.pythonstartup
