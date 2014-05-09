6d2e51effdbae196305ddb42921a6be6c43ce0fe
kashewnuts <bjzli.m08vo9kqs@gmail.com>
2014-05-09 15:15:24 +0900

    include local and add alias

--- setup.bat
+++ setup.bat
@@ -4,7 +4,7 @@ mklink %USERPROFILE%\.vimrc %DOTFILES%\.vimrc
 mklink %USERPROFILE%\.gvimrc %DOTFILES%\.gvimrc
 mklink /d %USERPROFILE%\vimfiles %DOTFILES%\.vim
 mklink /d %USERPROFILE%\.vim %DOTFILES%\.vim
-mklink /h %USERPROFILE%\.gitconfig %DOTFILES%\.gitconfig_win
+mklink %USERPROFILE%\.gitconfig %DOTFILES%\.gitconfig_win
 mklink %USERPROFILE%\.gitignore %DOTFILES%\.gitignore
 mklink %USERPROFILE%\.hgrc %DOTFILES%\.hgrc_win
 mklink %USERPROFILE%\.hgignore %DOTFILES%\.hgignore
