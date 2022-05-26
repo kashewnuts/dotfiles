24fe88cb0be27fdfbe47521ceadd60c75a9581ca
Kashun YOSHIDA <1122795+kashewnuts@users.noreply.github.com>
2022-05-26 22:44:02 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 python3 setup.py
-sudo apt-get update; apt-get -y upgrade; apt-get install -y vim tig
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y vim tig
